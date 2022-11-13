776020976eb244371b0c46a4eba68f5e27975252
SaicereT <justintrejos50@yahoo.com>
2022-11-13 01:02:17 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,80 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { read } from "@popperjs/core";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//Click for new card
+document.querySelector("#newCard").addEventListener("click", generateCard);
+
+//Timer for new card
+setInterval(generateCard, 10000);
+
+//Change width
+const card = document.querySelector(".card");
+
+document.querySelector("#width").addEventListener("change", changeWidth);
+
+function changeWidth() {
+  let widthInput = document.querySelector("#width").value;
+  let widthVal = document.querySelector("#width").value + "px";
+  if (widthInput > 0) {
+    card.style.width = widthVal;
+  } else {
+    card.style.width = "400px";
+  }
+}
+
+//Change height
+document.querySelector("#height").addEventListener("change", changeHeight);
+
+function changeHeight() {
+  let heightInput = document.querySelector("#height").value;
+  let heightVal = document.querySelector("#height").value + "px";
+  if (heightInput > 0) {
+    card.style.height = heightVal;
+  } else {
+    card.style.height = "600px";
+  }
+}
+
+function generateCard() {
+  let suitVal = Math.floor(Math.random() * 4 + 1);
+  let numVal = Math.floor(Math.random() * 12 + 1);
+  let topSuit = document.querySelector("#topSuit");
+  let cardNum = document.querySelector("#cardNum");
+  let botSuit = document.querySelector("#botSuit");
+
+  if (suitVal == 1) {
+    topSuit.style.color = "black";
+    botSuit.style.color = "black";
+    topSuit.innerHTML = "♠";
+    botSuit.innerHTML = "♠";
+  } else if (suitVal == 2) {
+    topSuit.style.color = "red";
+    botSuit.style.color = "red";
+    topSuit.innerHTML = "♥";
+    botSuit.innerHTML = "♥";
+  } else if (suitVal == 3) {
+    topSuit.style.color = "black";
+    botSuit.style.color = "black";
+    topSuit.innerHTML = "♣";
+    botSuit.innerHTML = "♣";
+  } else if (suitVal == 4) {
+    topSuit.style.color = "red";
+    botSuit.style.color = "red";
+    topSuit.innerHTML = "♦";
+    botSuit.innerHTML = "♦";
+  }
+
+  if (numVal == 13) {
+    cardNum.innerHTML = "K";
+  } else if (numVal == 12) {
+    cardNum.innerHTML = "Q";
+  } else if (numVal == 11) {
+    cardNum.innerHTML = "J";
+  } else if (numVal == 1) {
+    cardNum.innerHTML = "A";
+  } else {
+    cardNum.innerHTML = numVal;
+  }
+}
